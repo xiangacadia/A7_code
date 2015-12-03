7bf0490a15809797aff77ef9b0847e991531e14a
xjiang <xjiang@xjiangs-MacBook-Pro-2.local>
2015-12-02 21:40:18 -0400

    fixing test likelihood

--- news.m
+++ news.m
@@ -29,7 +29,7 @@ likelihood_train = matrix_train * logF + (1 - matrix_train)*logF0;
 [~,train_pred] = max(likelihood_train,[], 2);
 mean(labels_train==train_pred)
 
-likelihood = matrix_test * logF;
+likelihood = matrix_test * logF + (1 - matrix_test)*logF0;
 [~, test_pred] = max(likelihood,[], 2);
 
 mean(labels_test==test_pred)
